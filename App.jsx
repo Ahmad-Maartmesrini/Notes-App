ef7e40b18db3e9463d60bc335febc6e817fafb8c
Ahmad Maartmesrini <Ahmad-Maartmesrini@outlook.com>
2024-02-15 10:01:14 +0300

    update Note

--- App.jsx
+++ App.jsx
@@ -5,19 +5,18 @@ import Split from "react-split";
 import { onSnapshot, addDoc, doc, deleteDoc, setDoc } from "firebase/firestore";
 import { notesCollection, db } from "./firebase";
 
+
 export default function App() {
+
   const [notes, setNotes] = React.useState([]);
   const [currentNoteId, setCurrentNoteId] = React.useState("");
-  const [tempNoteText, setTempNoteText] = React.useState("");
 
   const currentNote =
     notes.find((note) => note.id === currentNoteId) || notes[0];
 
-  const sortedNotes = notes.sort((a, b) => b.updatedAt - a.updatedAt);
-
   React.useEffect(() => {
     const unsubscribe = onSnapshot(notesCollection, function (snapshot) {
-      console.log("things are changing");
+      // Sync up our local notes array with the snapshot data
       const notesArr = snapshot.docs.map((doc) => ({
         ...doc.data(),
         id: doc.id,
@@ -33,17 +32,9 @@ export default function App() {
     }
   }, [notes]);
 
-  React.useEffect(() => {
-    if (currentNote) {
-      setTempNoteText(currentNote.body);
-    }
-  }, [currentNote]);
-
   async function createNewNote() {
     const newNote = {
       body: "# Type your markdown note's title here",
-      createdAt: Date.now(),
-      updatedAt: Date.now(),
     };
     const newNoteRef = await addDoc(notesCollection, newNote);
     setCurrentNoteId(newNoteRef.id);
@@ -51,11 +42,7 @@ export default function App() {
 
   async function updateNote(text) {
     const docRef = doc(db, "notes", currentNoteId);
-    await setDoc(
-      docRef,
-      { body: text, updatedAt: Date.now() },
-      { merge: true }
-    );
+    await setDoc(docRef, { body: text }, { merge: true });
   }
 
   async function deleteNote(noteId) {
@@ -68,16 +55,13 @@ export default function App() {
       {notes.length > 0 ? (
         <Split sizes={[30, 70]} direction="horizontal" className="split">
           <Sidebar
-            notes={sortedNotes}
+            notes={notes}
             currentNote={currentNote}
             setCurrentNoteId={setCurrentNoteId}
             newNote={createNewNote}
             deleteNote={deleteNote}
           />
-          <Editor
-            tempNoteText={tempNoteText}
-            setTempNoteText={setTempNoteText}
-          />
+          <Editor currentNote={currentNote} updateNote={updateNote} />
         </Split>
       ) : (
         <div className="no-notes">
