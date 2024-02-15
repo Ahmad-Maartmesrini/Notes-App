93091a42965188c1173b9b8189757a7b1047f5a0
Ahmad Maartmesrini <Ahmad-Maartmesrini@outlook.com>
2024-02-15 11:42:28 +0300

    add warning message

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -2,33 +2,37 @@ import React from "react";
 // import "react-mde/lib/styles/css/react-mde-all.css";
 
 export default function Sidebar(props) {
-    const noteElements = props.notes.map((note, index) => (
-        <div key={note.id}>
-            <div
-                
-                className={`title ${
-                    note.id === props.currentNote.id ? "selected-note" : ""
-                }`}
-                onClick={() => props.setCurrentNoteId(note.id)}
-            >
-                <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
-                <button 
-                    className="delete-btn"
-                    onClick={() => props.deleteNote(note.id)}
-                >
-                    <i className="gg-trash trash-icon"></i>
-                </button>
-            </div>
-        </div>
-    ))
+  const noteElements = props.notes.map((note, index) => (
+    <div key={note.id}>
+      <div
+        className={`title ${
+          note.id === props.currentNote.id ? "selected-note" : ""
+        }`}
+        onClick={() => props.setCurrentNoteId(note.id)}
+      >
+        <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
+        <button
+          className="delete-btn"
+          onClick={() => props.deleteNote(note.id)}
+        >
+          <i className="gg-trash trash-icon"></i>
+        </button>
+      </div>
+    </div>
+  ));
 
-    return (
-        <section className="pane sidebar">
-            <div className="sidebar--header">
-                <h3>Notes</h3>
-                <button className="new-note" onClick={props.newNote}>+</button>
-            </div>
-            {noteElements}
-        </section>
-    )
+  return (
+    <section className="pane sidebar">
+      <h6 className="warning">
+        <i>Anyone can Edite these Notes!</i>
+      </h6>
+      <div className="sidebar--header">
+        <h3>Notes</h3>
+        <button className="new-note" onClick={props.newNote}>
+          +
+        </button>
+      </div>
+      {noteElements}
+    </section>
+  );
 }
